141386fe2afc2e24399c554ac05e2d69c08464ce
shen <995443908@qq.com>
2018-09-12 14:19:31 +0800

    1

--- thread/src/com/github/study/httpclient/SchoolNetDemo.java
+++ thread/src/com/github/study/httpclient/SchoolNetDemo.java
@@ -1,5 +1,6 @@
 package com.github.study.httpclient;
 
+import com.github.study.util.RegexSpiderUtil;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -7,28 +8,21 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.cookie.CookieSpecProvider;
-import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.cookie.BasicClientCookie;
-import org.apache.http.impl.cookie.BestMatchSpecFactory;
-import org.apache.http.impl.cookie.BrowserCompatSpecFactory;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.util.EntityUtils;
+
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 import java.io.FileOutputStream;
@@ -41,15 +35,12 @@ import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
- *
- *  学信网爬虫
+ * 学信网爬虫
  * Created by pc on 2018/9/7.
  */
-public class HCTest {
+public class SchoolNetDemo {
 
     static CookieStore cookieStore = null;
     static HttpClientContext context = null;
@@ -96,8 +87,8 @@ public class HCTest {
 //        execution: e2s1
 //        _eventId: submit
 //        submit: 登  录
-        formparams.add(new BasicNameValuePair("username", "383984883@qq.com"));
-        formparams.add(new BasicNameValuePair("password", "shen86312971"));
+        formparams.add(new BasicNameValuePair("username", "13849893919"));
+        formparams.add(new BasicNameValuePair("password", "lu123456"));
         formparams.add(new BasicNameValuePair("lt", "LT-1083756-vjdQiFBTWJG29UceUDRdWgvr1pfl1p"));
         formparams.add(new BasicNameValuePair("execution", "e2s1"));
         formparams.add(new BasicNameValuePair("_eventId", "submit"));
@@ -151,21 +142,6 @@ public class HCTest {
         return HttpClients.createDefault();
     }
 
-    private static void login2() {
-        try {
-
-            System.out.println("444");
-            HttpClient client = HttpClients.createDefault();
-            HttpGet httpGet = new HttpGet("http://hzq-api.zhiyun88u.com/tmind/v1/ious?limit=20&offset=0&is_borrow=");
-            HttpResponse response = client.execute(httpGet);
-            System.out.println("3333" + response.getStatusLine());
-            HttpEntity resEntity = response.getEntity();
-            String message = EntityUtils.toString(resEntity, "utf-8");
-            System.out.println("222" + message);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void getPage() throws IOException {
         // 创建默认的httpClient实例.
@@ -177,41 +153,37 @@ public class HCTest {
 //                .setRedirectStrategy(new LaxRedirectStrategy())
 //                .build();
         // 创建httppost
-        CloseableHttpClient httpclient =HttpClients.createDefault();
+        CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet("https://account.chsi.com.cn/passport/login");
         httpGet.setHeader("User-Agent", "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36");
         CloseableHttpResponse response = httpclient.execute(httpGet);
         HttpEntity entity = response.getEntity();
 
-        //cookie
-        String setCookie = response.getHeaders("Set-Cookie")[0].getValue();
 
-        System.out.println("JSESSIONID:" + setCookie);
-
-        String page=EntityUtils.toString(entity, "UTF-8");
+        String page = EntityUtils.toString(entity, "UTF-8");
         System.out.println("--------------------------------------");
-        System.out.println("Response content execution: " + getStr( "name=\"execution\" value=(.*?)/>",page));
-        System.out.println("Response content lt: " + getStr("name=\"lt\" value=(.*?)/>",page));
+        System.out.println("Response content execution: " + RegexSpiderUtil.getStr("name=\"execution\" value=(.*?)/>", page));
+        System.out.println("Response content lt: " + RegexSpiderUtil.getStr("name=\"lt\" value=(.*?)/>", page));
         System.out.println("--------------------");
 
-        String postUrl="https://account.chsi.com.cn/passport/login";
+        String postUrl = "https://account.chsi.com.cn/passport/login";
         // 创建参数队列
         List<NameValuePair> formparams = new ArrayList<NameValuePair>();
 
-        formparams.add(new BasicNameValuePair("username", "383984883@qq.com"));
-        formparams.add(new BasicNameValuePair("password", "shen86312971"));
-        formparams.add(new BasicNameValuePair("lt", getStr("name=\"lt\" value=(.*?)/>", page)));
-        formparams.add(new BasicNameValuePair("execution", getStr( "name=\"execution\" value=(.*?)/>",page)));
+        formparams.add(new BasicNameValuePair("username", "13849893919"));
+        formparams.add(new BasicNameValuePair("password", "lu123456"));
+        formparams.add(new BasicNameValuePair("lt", RegexSpiderUtil.getStr("name=\"lt\" value=(.*?)/>", page)));
+        formparams.add(new BasicNameValuePair("execution", RegexSpiderUtil.getStr("name=\"execution\" value=(.*?)/>", page)));
         formparams.add(new BasicNameValuePair("_eventId", "submit"));
         formparams.add(new BasicNameValuePair("submit", "登  录"));
-        postForm(postUrl,httpclient,formparams);
+        postForm(postUrl, httpclient, formparams);
     }
 
 
     /**
      * post方式提交表单（模拟用户登录请求）
      */
-    public static void postForm(String url, CloseableHttpClient httpclient, List<NameValuePair> formparams  ) {
+    public static void postForm(String url, CloseableHttpClient httpclient, List<NameValuePair> formparams) {
         // 创建httppost
         HttpPost httppost = new HttpPost(url);
 
@@ -220,23 +192,23 @@ public class HCTest {
             uefEntity = new UrlEncodedFormEntity(formparams, "UTF-8");
             httppost.setEntity(uefEntity);
             httppost.setHeader("User-Agent", "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36");
-            httppost.setHeader("Content-Type","application/x-www-form-urlencoded");
+            httppost.setHeader("Content-Type", "application/x-www-form-urlencoded");
             CloseableHttpResponse response = httpclient.execute(httppost);
 
 
             try {
                 HttpEntity entity = response.getEntity();
-                String resultPage=EntityUtils.toString(entity, "UTF-8");
+                String resultPage = EntityUtils.toString(entity, "UTF-8");
                 if (entity != null) {
                     System.out.println("--------------------------------------");
                     System.out.println("###LOGIN Response content execution: " + resultPage);
                     System.out.println("--------------------------------------");
-                    if(resultPage.trim().isEmpty()) {
-                    //if (response.getStatusLine().getStatusCode() == 200) {
+                    if (resultPage.trim().isEmpty()) {
+                        //if (response.getStatusLine().getStatusCode() == 200) {
                         getPage("https://my.chsi.com.cn/archive/gdjy/xj/show.action", httpclient);
-                    }else{
-                        String resultMsg=getStr( "<div id=\"status\" class=\"errors\">(.*?)</div>",resultPage);
-                        System.out.println("rrrrrrrrrrrrr="+resultMsg);
+                    } else {
+                        String resultMsg = RegexSpiderUtil.getStr("<div id=\"status\" class=\"errors\">(.*?)</div>", resultPage);
+                        System.out.println("rrrrrrrrrrrrr=" + resultMsg);
                     }
 
                     //-------------
@@ -275,24 +247,25 @@ public class HCTest {
         CloseableHttpResponse response = httpclient.execute(httpGet);
 
         HttpEntity entity = response.getEntity();
-        String page=EntityUtils.toString(entity, "UTF-8");
+        String page = EntityUtils.toString(entity, "UTF-8");
 
         int code = response.getStatusLine().getStatusCode();
-        String newuri="";
+        String newuri = "";
         if (code == 302) {
             Header header = response.getFirstHeader("location"); // 跳转的目标地址是在 HTTP-HEAD 中的
             newuri = header.getValue(); // 这就是跳转后的地址，再向这个地址发出新申请，以便得到跳转后的信息是啥。
-            getPage2(newuri,httpclient);
+            getPage2(newuri, httpclient);
             return;
         }
-        System.out.println("--------------------------------------"+page);
-        String imgUrl=getStr( "/gdjy/photo/show.action(.*?)\"",page,2);
+        System.out.println("--------------------------------------" + page);
+        String imgUrl = RegexSpiderUtil.getStr("/gdjy/photo/show.action(.*?)\"", page, 2);
         System.out.println("--------------------------------------");
         System.out.println("END############getPage Response content execution:https://my.chsi.com.cn/archive/gdjy/photo/show.action" + imgUrl);
-        downloadImg("https://my.chsi.com.cn/archive/gdjy/photo/show.action" + imgUrl,httpclient);
+        downloadImg("https://my.chsi.com.cn/archive/gdjy/photo/show.action" + imgUrl, httpclient);
 //        System.out.println("--------------------"+page);
         //https://my.chsi.com.cn/archive/gdjy/photo/show.action?pid=4d2ab66f312b85f6a17b56d85a309d27";
     }
+
     public static void getPage2(String url, CloseableHttpClient httpclient) throws IOException {
         HttpGet httpGet = new HttpGet(url);
         httpGet.setHeader("User-Agent", "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36");
@@ -300,16 +273,17 @@ public class HCTest {
         CloseableHttpResponse response = httpclient.execute(httpGet);
 
         HttpEntity entity = response.getEntity();
-        String page=EntityUtils.toString(entity, "UTF-8");
+        String page = EntityUtils.toString(entity, "UTF-8");
 
-        String imgUrl=getStr( "/gdjy/photo/show.action(.*?)\"",page,2);
+        String imgUrl = RegexSpiderUtil.getStr("/gdjy/photo/show.action(.*?)\"", page, 2);
         System.out.println("--------------------------------------");
         System.out.println("END############getPage Response content execution:https://my.chsi.com.cn/archive/gdjy/photo/show.action" + imgUrl);
-        downloadImg("https://my.chsi.com.cn/archive/gdjy/photo/show.action" + imgUrl,httpclient);
+        downloadImg("https://my.chsi.com.cn/archive/gdjy/photo/show.action" + imgUrl, httpclient);
 //        System.out.println("--------------------"+page);
         //https://my.chsi.com.cn/archive/gdjy/photo/show.action?pid=4d2ab66f312b85f6a17b56d85a309d27";
     }
-    private static void downloadImg(String url,CloseableHttpClient httpclient) throws IOException {
+
+    private static void downloadImg(String url, CloseableHttpClient httpclient) throws IOException {
         HttpGet httpGet = new HttpGet(url);
         httpGet.setHeader("User-Agent", "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36");
 
@@ -318,66 +292,21 @@ public class HCTest {
         HttpEntity entity = response.getEntity();
 
         byte[] data = EntityUtils.toByteArray(entity);
-
+        String imgName = System.currentTimeMillis() + ".jpg";
+        String path="d:/download/xxw/"+imgName;
         //图片存入磁盘
-        FileOutputStream fos = new FileOutputStream("d:/mpl.jpg");
+        FileOutputStream fos = new FileOutputStream(path);
         fos.write(data);
         fos.close();
 
         System.out.println("图片下载成功!!!!");
     }
 
-    public static void setContext() {
-        System.out.println("----setContext");
-        context = HttpClientContext.create();
-        Registry<CookieSpecProvider> registry = RegistryBuilder
-                .<CookieSpecProvider> create()
-                .register(CookieSpecs.BEST_MATCH, new BestMatchSpecFactory())
-                .register(CookieSpecs.BROWSER_COMPATIBILITY,
-                        new BrowserCompatSpecFactory()).build();
-        context.setCookieSpecRegistry(registry);
-        context.setCookieStore(cookieStore);
+    public static void main(String[] args) throws IOException {
+        getPage();
     }
-    public static void setCookieStore(HttpResponse httpResponse) {
-        System.out.println("----setCookieStore");
-        cookieStore = new BasicCookieStore();
-        // JSESSIONID
-        String setCookie = httpResponse.getFirstHeader("Set-Cookie")
-                .getValue();
-        String JSESSIONID = setCookie.substring("JSESSIONID=".length(),
-                setCookie.indexOf(";"));
-        System.out.println("JSESSIONID:" + JSESSIONID);
-        // 新建一个Cookie
-        BasicClientCookie cookie = new BasicClientCookie("JSESSIONID",
-                JSESSIONID);
-        cookie.setVersion(0);
-        cookie.setDomain("127.0.0.1");
-        cookie.setPath("/CwlProClient");
-        // cookie.setAttribute(ClientCookie.VERSION_ATTR, "0");
-        // cookie.setAttribute(ClientCookie.DOMAIN_ATTR, "127.0.0.1");
-        // cookie.setAttribute(ClientCookie.PORT_ATTR, "8080");
-        // cookie.setAttribute(ClientCookie.PATH_ATTR, "/CwlProWeb");
-        cookieStore.addCookie(cookie);
-    }
-    private static  String getStr(String rgex, String str){
 
-        Pattern pattern = Pattern.compile(rgex);// 匹配的模式
-        Matcher m = pattern.matcher(str);
-        m.find();
-       return m.group(1).replaceAll("\"","").trim();
-    }
-    private static  String getStr(String rgex, String str,int num){
 
-        Pattern pattern = Pattern.compile(rgex);// 匹配的模式
-        Matcher m = pattern.matcher(str);
-        for (int i=0;i<num;i++){
-            m.find();
-        }
-        return m.group(1).replaceAll("\"","").trim();
 
-    }
-    public static void main(String[] args) throws Exception {
-       getPage();
-    }
 }
 
---
+++ thread/src/com/github/study/httpclient/ZfbDemo.java
@@ -0,0 +1,135 @@
+package com.github.study.httpclient;
+
+import com.github.study.util.RegexSpiderUtil;
+import com.github.study.util.ZxingUtil;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by pc on 2018/9/11.
+ */
+public class ZfbDemo {
+
+    private static final String QRCODE_STATUS_WAITING = "waiting";
+    //扫码成功
+    private static final String QRCODE_STATUS_SCANNED = "scanned";
+    //确认登录
+    private static final String QRCODE_STATUS_CONFIRMED = "confirmed";
+
+    private static void getZfEwm() throws Exception {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://auth.alipay.com/login/index.htm");
+        httpGet.setHeader("User-Agent", "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        if (response.getStatusLine().getStatusCode() == 200) {
+            HttpEntity entity = response.getEntity();
+
+            String page = EntityUtils.toString(entity, "UTF-8");
+            getLoginParams(page);
+            //System.out.println(getStr("barcode: \"(.*?)\"", page));
+            System.out.println(ZxingUtil.createQrCode(RegexSpiderUtil.getStr("barcode: \"(.*?)\"", page)));
+            //生成二维码后监听二维码状态
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            int i = 0;
+            params.add(new BasicNameValuePair("securityId", RegexSpiderUtil.getStr("securityId=(.*?)\"", page).toString().replaceAll("%257C", "|")));
+            while (true) {
+                System.out.println(RegexSpiderUtil.getStr("securityId=(.*?)\"", page));
+                params.add(new BasicNameValuePair("_callback", "light.request._callbacks.callback" + i));
+                i += 1;
+                Thread.sleep(3000);
+                if (checkStatus(params, httpclient,page)) {
+                    break;
+                }
+            }
+        }
+    }
+
+    private static boolean checkStatus(List<NameValuePair> params, CloseableHttpClient httpclient,String indexPage) throws Exception {
+        String paramsStr = EntityUtils.toString(new UrlEncodedFormEntity(params, "UTF-8"));
+        HttpGet httpGet = new HttpGet("https://securitycore.alipay.com/barcode/barcodeProcessStatus.json" + "?" + paramsStr);
+        httpGet.setHeader("User-Agent", "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        HttpEntity entity = response.getEntity();
+        if (response.getStatusLine().getStatusCode() == 200) {
+            String page = EntityUtils.toString(entity, "UTF-8");
+            //System.out.println(getStr("barcode: \"(.*?)\"", page));
+            System.out.println(page);
+            //如果确认了 手机信息
+            if (page.indexOf(ZfbDemo.QRCODE_STATUS_CONFIRMED) != -1) {
+                login(httpclient,indexPage);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void login(CloseableHttpClient httpclient,String indexPage) throws Exception{
+        List<NameValuePair> loginparams = getLoginParams(indexPage);
+        HttpEntity reqEntity = new UrlEncodedFormEntity(loginparams, "utf-8");
+        HttpPost post = new HttpPost("https://authet15.alipay.com/login/index.htm");
+        post.setEntity(reqEntity);
+        CloseableHttpResponse response = httpclient.execute(post);
+
+        getInformation(httpclient);
+
+    }
+
+    private static List<NameValuePair> getLoginParams(String indexPage) {
+        List<NameValuePair> loginparams =new ArrayList<NameValuePair>();
+        String support=RegexSpiderUtil.getStr("<input type=\"hidden\" name=\"support\" value = \"(.*?)\"", indexPage);
+        String CtrlVersion=RegexSpiderUtil.getStr("<input type=\"hidden\" name=\"CtrlVersion\" value = \"(.*?)\"", indexPage);
+        String loginScene=RegexSpiderUtil.getStr("<input type=\"hidden\" name=\"loginScene\" value = \"(.*?)\"", indexPage);
+        String rds_form_token=RegexSpiderUtil.getStr("<input type=\"hidden\" value=\"(.*?)\" name=\"rds_form_token", indexPage);
+        String qrCodeSecurityId= RegexSpiderUtil.getStr("securityId=(.*?)\"", indexPage).toString().replaceAll("%257C", "|");
+        String J_aliedit_key_hidn=RegexSpiderUtil.getStr("<input type=\"hidden\" name=\"J_aliedit_key_hidn\" value=\"(.*?)\"", indexPage);
+        String J_aliedit_uid_hidn=RegexSpiderUtil.getStr("<input type=\"hidden\" name=\"J_aliedit_uid_hidn\" value=\"(.*?)\"", indexPage);
+        String alieditUid=RegexSpiderUtil.getStr("<input type=\"hidden\" id=\"alieditUid\" name=\"alieditUid\" value=\"(.*?)\"", indexPage);
+        String passwordSecurityId=RegexSpiderUtil.getStr("<input type=\"hidden\" name=\"passwordSecurityId\" value=\"(.*?)\"", indexPage);
+
+        loginparams.add(new BasicNameValuePair("support",support));
+        loginparams.add(new BasicNameValuePair("CtrlVersion",CtrlVersion));
+        loginparams.add(new BasicNameValuePair("loginScene","index"));
+        loginparams.add(new BasicNameValuePair("rds_form_token",rds_form_token));
+        loginparams.add(new BasicNameValuePair("json_tk","json_tk"));
+        loginparams.add(new BasicNameValuePair("method","qrCodeLogin"));
+        loginparams.add(new BasicNameValuePair("qrCodeSecurityId",qrCodeSecurityId));
+        loginparams.add(new BasicNameValuePair("qrCodeSecurityId",J_aliedit_key_hidn));
+        loginparams.add(new BasicNameValuePair("qrCodeSecurityId",J_aliedit_uid_hidn));
+        loginparams.add(new BasicNameValuePair("qrCodeSecurityId",alieditUid));
+        loginparams.add(new BasicNameValuePair("qrCodeSecurityId",passwordSecurityId));
+        return loginparams;
+    }
+
+    private static void getInformation(CloseableHttpClient httpclient) throws IOException {
+       // HttpGet httpGet = new HttpGet("https://custweb.alipay.com/account/index.htm");
+        HttpGet httpGet = new HttpGet("https://personalweb.alipay.com/account/payAndCredit.htm");
+
+        httpGet.setHeader("User-Agent", "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        HttpEntity entity = response.getEntity();
+        String page = EntityUtils.toString(entity, "UTF-8");
+        System.out.println(page);
+        if (response.getStatusLine().getStatusCode() == 200) {
+
+            //String name=RegexSpiderUtil.getStr("<span id=\"username\">(.*?)</span>", page);
+            //System.out.println(page);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        getZfEwm();
+    }
+}
